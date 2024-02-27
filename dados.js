8a62742d5af5aaf266a53b84d1ab491781677646
TiagoFelipe-89 <161519948+TiagoFelipe-89@users.noreply.github.com>
2024-02-27 17:25:22 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,400 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/churrasco/202206092107_UW51_i.jpg",
+            "name": "Beirute de frango",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 28
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/202208161704_B4Y2_i.jpg",
+            "name": "X- egg burguer",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 25.5
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/churrasco/202208161820_431C_i.jpg",
+            "name": "X- cheddar",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 23.5
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/churrasco/202208171639_E427_i.jpg",
+            "name": "Beirute de carne",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 29.9
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "./img/cardapio/churrasco/202209171204_6N48_i.jpg",
+            "name": "Combo barateiro",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 21.5
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/cardapio/churrasco/202302242138_V4VC_i.jpg",
+            "name": "Combo burguer kids",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 19.9
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/cardapio/churrasco/202302271508_EP50_i.jpg",
+            "name": "Combo oxente",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 12.9
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/churrasco/combo guarana.jpg",
+            "name": "Combo guaraná",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 25.5
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/churrasco/combo nordestino.jpg",
+            "name": "Combo nordestino",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 26
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/cardapio/churrasco/trio.jpg",
+            "name": "Combo X-tudo",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 28.5
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/cardapio/churrasco/como acai tradicional.jpg",
+            "name": "Combo açaí tradicional",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 24.5
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/cardapio/churrasco/combo trio perfeito.jpg",
+            "name": "Combo trio perfeito",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 36
+        }
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
+            "name": "Hamburguer",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 12
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
+            "name": "X-burguer",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 14
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
+            "name": "X- cheddar burguer",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 13.9
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "./img/cardapio/burguers/x-egg burguer.jpg",
+            "name": "X- egg burguer",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 15.5
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/x-tudo.jpg",
+            "name": "X-tudo",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 22
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/beirute de carne.jpg",
+            "name": "Beirute de carne",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 25.9
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img": "./img/cardapio/burguers/beirute de frango.jpg",
+            "name": "Beirute de frango",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 22
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "./img/cardapio/burguers/caruaru.jpg",
+            "name": "Artesanal caruaru",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 22
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "./img/cardapio/burguers/Garanhuns.jpg",
+            "name": "Artesanal garanhuns",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 23
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/suco.jpg",
+            "name": "Guaraçaí",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 10
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/suco 1.jpg",
+            "name": "Açaí c/ banana",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 10
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/sobremesas/amazonas.jpg",
+            "name": "Guaraná do amazonas",
+            "dsc": "Original Cannoli Pie",
+            "price": 7.5
+        },
+        
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/202202011544_YO23_i.jpg",
+            "name": "Refrigerante mini",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 4
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/202210180644_xnt0cy3qs4.jpg",
+            "name": "Água mineral",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 2
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/bebidas/agua c gas.jpg",
+            "name": "Água mineral c/ gás",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 3
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/bebidas/h2oh.jpg",
+            "name": "H2o",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 6.5
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cardapio/bebidas/limoneto.jpg",
+            "name": "H2o limoneto",
+            "dsc": "Margarita Mix",
+            "price": 6.5
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/bebidas/refrigerante lata.jpg",
+            "name": "Refrigerante lata",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 5
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/bebidas/antartica.jpg",
+            "name": "Antarctica 1L",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 7
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/bebidas/coca.jpg",
+            "name": "Coca-cola 1L",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 8.9
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/bebidas/monster.jpg",
+            "name": "Energético Monster",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 13
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/bebidas/suco polpa.jpg",
+            "name": "Suco de polpa",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 7.5
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/bebidas/202112161233_43U5_i.jpg",
+            "name": "Suco de laranja",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 8
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/bebidas/del valle.jpg",
+            "name": "Del valle",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 5.5
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/2-lou-malnatis-deep-dish-pizzas.bf0fe065d251a9cca3925b269d443a27.jpg",
+            "name": "Açaí 300ml.",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 14.9
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-thin-crust-pizza-4-pack.b928a2008eab50c65dc87e59b5952190.jpg",
+            "name": "Açaí c/ morango 300ml",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 13.9
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/acai c banana.jpg",
+            "name": "Açaí c/ banana",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 12.9
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/cucuacu c morango.jpg",
+            "name": "Cupuaçu c/ morango",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 16
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/pizzas/acai cupuacu.jpg",
+            "name": "Açaí puro",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 12
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/acai com morango amendoim leite condensado.jpg",
+            "name": "Açaí especial",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 14.9
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/acai com cupuacu banana e lc.jpg",
+            "name": "Cupuaçu c/ banana",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 14.5
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/cardapio/pizzas/acai monte o seu.jpg",
+            "name": "Açaí monte o seu",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 16
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/acai na tijela.jpg",
+            "name": "Açaí na tijela",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 14
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/acai na tigela 1.jpg",
+            "name": "Açaí c/ cupuaçu",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 18.5
+        },
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/202202211155_21I2_i.jpg",
+            "name": "Tapioca 2 recheios",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 14
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/202202220026_S2T8_i.jpg",
+            "name": "Tapioca cartola",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 13
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/202307102026_EW11_i.jpg",
+            "name": "Tapioca leite condensado",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 10
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/202307102030_7370_i.jpg",
+            "name": "Tapioca c/ 1 recheio",
+            "dsc": "Bone-in Rib Steak",
+            "price": 9
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/batata frita.jpg",
+            "name": "Batata simples",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 14
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/batata frita gourmet.jpg",
+            "name": "Batata gourmet",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 18.9
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/Sem título.jpg",
+            "name": "Macaxeira na chapa",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 16
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/cuscuz 1 acompanhamento.jpg",
+            "name": "Cuscuz c/ 1 acompanhamento",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 12.9
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/cuscuz 2 acompanhamentos.jpg",
+            "name": "Cuscuz c/ 2 acompanhamentos",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 15
+        },
+    ]
+}
